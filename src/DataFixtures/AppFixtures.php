14c0b7b915f10e15137e44db4f9b40ba34e8eaed
khalil-git-dev <khalil02029@gmail.com>
2021-04-16 18:29:39 +0000

    [GCEL-4 : Authentification des utilisateurs(Proviseur,Censeur et Profeseur.....)]

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Role;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoderpass;
+    public function __construct(UserPasswordEncoderInterface $encoderpass)
+    {
+        $this->encoderpass = $encoderpass;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $role = new Role();
+        $role->setLibelle('SUP_ADMIN');
+        $manager->persist($role);
+
+        $role1 = new Role();
+        $role1->setLibelle('PROVISEUR');
+        $manager->persist($role1);
+
+        $role2 = new Role();
+        $role2->setLibelle('PROFESSEUR');
+        $manager->persist($role2);
+
+        $role3 = new Role();
+        $role3->setLibelle('ELEVE');
+        $manager->persist($role3);
+    
+        $supAdmin = new User();
+        $supAdmin->setUsername('supadmin@gmail.com');
+        $supAdmin->setPrenom('khalil');
+        $supAdmin->setNom('diop');
+        $supAdmin->setTelephone('777911628');
+        $supAdmin->setRole($role);
+        $supAdmin->setPassword($this->encoderpass->encodePassword($supAdmin, "supadmin"));
+
+        $manager->persist($supAdmin);
+        $manager->flush();
+
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Role", inversedBy="user")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $role;
+
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+     /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $telephone;
+
+    /**
+     * @ORM\Column(type="date")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->isActive = true;
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles()
+    {
+        // genere le role de l'utilsateur a partir de son role_id
+        return $this->getRoles = [('ROLE_'.strtoupper($this->getRole()->getLibelle()))];
+
+    }
+    
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getRole(): ?Role
+    {
+        return $this->role;
+    }
+
+    public function setRole(?Role $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}

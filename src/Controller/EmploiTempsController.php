109da9f46aede362f9713536f39824cf136b1aaa
Moussa <moussa@Moussas-MacBook-Pro.local>
2021-06-12 15:50:55 +0000

    comment

--- src/Controller/EmploiTempsController.php
+++ src/Controller/EmploiTempsController.php
@@ -2,24 +2,39 @@
 
 namespace App\Controller;
 
+use DateTime;
+use App\Entity\Date;
 use App\Entity\Cours;
+use App\Entity\Salle;
+use App\Entity\Classe;
+use App\Entity\Formateur;
+use App\Entity\Discipline;
+use App\Repository\ClasseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Controller\EmploiTempsController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 /**
  * @Route("/api", name="api_")
  */
 
 class EmploiTempsController extends AbstractController
 {
+    private $tokenStorage;
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
     /**
-     * @Route("/", name="a"),methods={"GET"})
+     * @Route("/addEmploiTemps", name="EmploiTemps"),methods={"POST"})
      */
-    public function liste()
+    public function add(ClasseRepository $repoClass, Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $userPasswordEncoder)
     {
         $rolesUser = $this->tokenStorage->getToken()->getUser()->getRoles()[0];
         if (!($rolesUser == "ROLE_SUP_ADMIN" || $rolesUser == "ROLE_PROVISEUR" || $rolesUser == "ROLE_INTENDANT")) {
@@ -29,7 +44,7 @@ class EmploiTempsController extends AbstractController
             ];
             return new JsonResponse($data, 401);
         }
-    // $values = json_decode($request->getContent());
+     $values = json_decode($request->getContent());
     // $jour = array('Dim.','Lun.','Mar.','Mer.','Jeu.','Ven.','Sam.');
     // $mois = array('Jan','Fév','Mar','Avr','Mai','Juin','Juil','Aoû','Sep','Oct','Nov','Déc');
 
@@ -38,30 +53,64 @@ class EmploiTempsController extends AbstractController
 
     //return $date;
  
-    $cours = new Cours();
-    $classe = new Classe();
-    $formateur = new User();
+    $cour = new Cours();
     $date = new Date();
     $date->setDateDebut(new \DateTime());
     $date->setDateFin(new \DateTime());
     $date->setDateEmmission(new \DateTime());
     $entityManager->persist($date);
-    $events = $calendar->findAll();
 
-    $rdvs = [];
+        //recuper les matieres
+        $repoMat=$this->getDoctrine()->getRepository(Discipline::class);
+        $matier=$repoMat->find($values->discipline);     
+       //recupere le formateur
+       $repofor= $this->getDoctrine()->getRepository(Formateur::class);
+    $formateur= $repofor->findBy(array('typeFor' => 'DESC'));
+    
+    $horaire= '';
+    switch ('typeFor'){
+	    case 'PES':
+           $horaire = 21;
 
-    foreach($events as $event){
-        $rdvs[] = [
-            'id' => $event->getId(),
-            'start' => $event->getStart()->format('Y-m-d H:i:s'),
-            'end' => $event->getEnd()->format('Y-m-d H:i:s'),
-            'title' => $event->getTitle(),
-            'description' => $event->getDescription(),
-            'backgroundColor' => $event->getBackgroundColor(),
-            'borderColor' => $event->getBorderColor(),
-            'textColor' => $event->getTextColor(),
-            'allDay' => $event->getAllDay(),
-        ];
+        break;
+        case 'VOCATAIRE':
+            # code...
+        break;
+        case 'PROFESSEUR CONTRACTUEL':
+            # code...
+        break;
+        }  
+        dd($formateur);
+       //Affectation d'un formateur a un cours
+        //   $formateur =  $repofor->find($values->idformateur);
+        //   $classe = $repoClass->find($values->idClasse);
+      
+       // recupere sall
+       $reposal= $this->getDoctrine()->getRepository(Salle::class);
+       $salle=$reposal->find($values->salle);
+        dd($salle);
+       foreach($values->classes as $key => $value){
+
+        $cour->addClasse($repoClass->find($value));
+        $entityManager->persist($cour);
+
+     }
+
+       $cour->setDetailCr($values->detail);
+       $cour->setLibelleCr($values->libelle);
+       $cour->setDureeCr(new \DateTime($values->duree));
+       $cour->setDiscipline($matier);
+       $cour->setFormateur($formateur);
+       $cour->setDateCours($date);
+       $cour->setSalle($salle);
+       $entityManager->persist($cour);
+       $entityManager->flush();
+       $data = [
+        'status' => 201,
+        'message' => "Cours ajouter avec succes."
+    ];
+    return new JsonResponse($data, 201); 
+
     }
- }
+ 
 }  

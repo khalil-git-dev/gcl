8117c5610000a382b18e1de1d044c9eac4678ba0
khalil-git-dev <khalil02029@gmail.com>
2021-06-13 21:24:56 +0000

    ok

---
+++ public/index2.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gcl_sa</title>
+    <link rel="stylesheet" type="text/css" href="../../assets/css/bootstrap.min.css">
+    <link href="../../lib/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/iofrm-style.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/iofrm-theme17.css">
+</head>
+
+<body>
+    <div class="form-body without-side">
+        <div class="row">
+            <div class="img-holder">
+                <div class="bg"></div>
+            </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <div class="website-logo">
+                            <a href="index.html">
+                                <div class="logo">
+                                    <img class="logo-size" src="../../assets/img/logo.png" alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <!-- <div class="info-holder">
+                            <img src="images/graphic3.svg" alt="">
+                        </div> -->
+                        <h3>Se Connecter </h3>
+                        <p>Bienvenue sur Votre plateforme de Gestion des Collèges et Lycée</p>
+                        <form>
+                            <input class="form-control" type="text" name="username" placeholder="Identification" required>
+                            <input class="form-control" type="password" name="password" placeholder="Mot de passe" required>
+                            <div class="form-button">
+                                <button id="submit" type="submit" class="ibtn">Se Connecter</button> <a href="forget17.html">Mot de passe oublié ?</a>
+                            </div>
+                        </form>
+                        <!-- <div class="page-links">
+                            <a href="register17.html">Register new account</a>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../../assets/js/jsform/jquery.min.js"></script>
+    <script src="../../assets/js/jsform/popper.min.js"></script>
+    <script src="../../assets/js/jsform/bootstrap.min.js"></script>
+    <script src="../../assets/js/jsform/main.js"></script>
+</body>
+
+</html>

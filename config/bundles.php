72857c194a27d2605ba1d7343a5d568f13724f1b
Moussa <moussa@Moussas-MacBook-Pro.local>
2021-06-22 13:29:50 +0000

    ajou

--- config/bundles.php
+++ config/bundles.php
@@ -16,9 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-<<<<<<< HEAD
-  // Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-=======
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => false],
->>>>>>> 13eca0ab7881c6c2242f707be6982265dcbb9598
 ];

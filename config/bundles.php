8f33fce4258a7bbd56aa307ddf4ac378649eea59
Moussa <moussa@Moussas-MacBook-Pro.local>
2021-07-13 17:33:36 +0000

    ajou

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => false],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => false],
 ];
